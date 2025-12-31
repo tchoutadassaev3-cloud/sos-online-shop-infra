98665ac5445c01cf124ad13c97da6b826d657419
tchoutadassaev3-cloud <tchoutadassaev3@gmail.com>
2025-12-31 17:18:10 +0100

    Define outputs for infrastructure resources
    Add outputs for web server IP, S3 bucket name, and website URL.

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,1 +1,14 @@
+output "web_server_public_ip" {
+  description = "Public IP address of the SOS web server"
+  value       = aws_instance.web.public_ip
+}
 
+output "s3_bucket_name" {
+  description = "Name of the S3 bucket for static assets"
+  value       = aws_s3_bucket.assets.bucket
+}
+
+output "website_url" {
+  description = "URL to access the SOS Online Shop"
+  value       = "http://${aws_instance.web.public_ip}"
+}
