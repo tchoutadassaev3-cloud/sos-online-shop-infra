bc9a603fa55127fd816c5a765b790ab42629cb3e
tchoutadassaev3-cloud <tchoutadassaev3@gmail.com>
2025-12-31 16:27:54 +0100

    Add variables for AWS region, project name, and environment

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,1 +1,17 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
 
+variable "project_name" {
+  description = "Project name for resource tagging"
+  type        = string
+  default     = "sos-online-shop"
+}
+
+variable "environment" {
+  description = "Deployment environment"
+  type        = string
+  default     = "dev"
+}
